9e5502071ac2e11af92270dfd21cab3a1f0eff40
Zolton <gmzesq@gmail.com>
2020-01-23 13:33:41 -0500

    Set up user router endpoint

--- api/server.js
+++ api/server.js
@@ -1,12 +1,20 @@
+// Imports
 const express = require ("express")
 const server = express ()
 const helmet = require("helmet")
 const cors = require("cors")
 const jwt = require("jsonwebtoken")
 
-server.use(helmet())
+// Location of routers
+const userRouter = require("../userRouter/userRouter")
+
+// Global server settings
 server.use(express.json())
+server.use(helmet())
 server.use(cors())
+
+// Routers in use
+server.use("/users", userRouter)
 
 server.get("/", (req, res) => {
     res.status(200).json({Hello: "from server"})
