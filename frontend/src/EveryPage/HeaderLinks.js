b86ce4d3d8defea2250cde3001a8f392b8753ca6
Zolton <gmzesq@gmail.com>
2020-01-24 18:56:04 -0500

    junk

--- frontend/src/EveryPage/HeaderLinks.js
+++ frontend/src/EveryPage/HeaderLinks.js
@@ -29,12 +29,7 @@ function HeaderLink() {
       <a href={baseURL + "contact"} className="navLink">
         Contact Us
       </a>
-      <Link to="/" className="navLink">
-        Registration
-      </Link>
-      <Link to="/login" className="navLink">
-        Login
-      </Link>
+
     </div>
   );
 }
