ed212522fcff728eea9c810d70a411c2f0792a31
Zolton <gmzesq@gmail.com>
2020-01-25 11:46:14 -0500

    Yellow warnings crashing netlify

--- frontend/src/Login/FormikLogin.js
+++ frontend/src/Login/FormikLogin.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { withFormik, Field, Form } from "formik";
 import * as Yup from "yup";
--- frontend/src/PublicPages/404.js
+++ frontend/src/PublicPages/404.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import HeaderLink from "../EveryPage/HeaderLinks";
 import FooterLinks from "../EveryPage/FooterLinks";
 
