c78fccdf6acf0739bd163f60c2f1b5b12a04b19f
Zolton <gmzesq@gmail.com>
2020-01-27 16:51:30 -0500

    Heroku messing with file structure; duplicated everything

--- database/db-config.js
+++
@@ -1,7 +0,0 @@
-const knex = require("knex")
-const knexConfig = require("../knexfile")
-
-const dbENV = process.env.DB_ENV || "development"
-module.exports = knex(knexConfig[dbENV])
-
-// heroku addons:rename postgresql-sinuous-70465 nihongo2go-database -a nihongo2go
