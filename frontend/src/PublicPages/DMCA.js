d4eedbbb33be9ad0f9beff6c8ef18c358dfd8706
Zolton <gmzesq@gmail.com>
2020-02-07 09:26:27 -0500

    Getting back into the swing of things.  Added page for DMCA notices

---
+++ frontend/src/PublicPages/DMCA.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//import axiosWithAuth from "../security/AxiosWithAuth";
+
+function DMCA () {
+
+    return (<div>
+
+      <p>Hello from DMCA</p></div>
+    )
+}
+
+
+
+export default DMCA
