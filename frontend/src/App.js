9c27f73ef8c58c40e7044969b11ac3579d1db099
Zolton <gmzesq@gmail.com>
2020-02-21 12:31:40 -0500

    segregated routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import "./App.css";
+
+// Every page
 import HeaderLinks from "./EveryPage/HeaderLinks";
-import LandingPage from "./PublicPages/LandingPage";
+import FooterLinks from "./EveryPage/FooterLinks";
+
+// Login/Registration
 import Login from "./Login/Login";
 import Registration from "./Login/Registration";
-import PrivateRoute from "./Security/PrivateRoute";
-import UserHome from "./Users/UserHome";
-import AboutUs from "./PublicPages/AboutUs";
+
+// Public Pages
 import Error404 from "./PublicPages/404";
-import FooterLinks from "./EveryPage/FooterLinks";
+import AboutUs from "./PublicPages/AboutUs";
+import ContactUs from "./PublicPages/ContactUs"
+import DMCA from "./PublicPages/DMCA"
+import LandingPage from "./PublicPages/LandingPage"
+import PrivateRoute from "./Security/PrivateRoute";
+
+// Private Routes - registers users only
+import UserHome from "./Users/UserHome";
+
+
+
 
 function App() {
   return (
@@ -20,6 +33,8 @@ function App() {
         <Route exact path="/about" component={AboutUs} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/404" component={Error404} />
+        <Route exact path="/contact" component={ContactUs} />
+        <Route exact path="/dmca" component={DMCA} />
         <Route exact path="/registration" component={Registration} />
         <Route exact path="/home" component={UserHome} />
         <FooterLinks />
