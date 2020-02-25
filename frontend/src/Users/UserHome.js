d1fb3fb23abdaa5b16ecac8e9e491b2ba31b15b1
Greg Zolton II <39355656+Zolton@users.noreply.github.com>
2020-02-25 16:04:54 -0500

    Update UserHome.js

--- frontend/src/Users/UserHome.js
+++ frontend/src/Users/UserHome.js
@@ -9,7 +9,7 @@ function UserHome() {
     axiosWithAuth()
       .get(`https://nihongo2go.netlify.com/quiz/allquestions`)
       .then(res => {
-        console.log(res.data);
+        console.log("this is res data", res.data);
         setQuizData(res.data);
       })
       .catch(rej => {
@@ -22,6 +22,7 @@ function UserHome() {
     <div className="test">
       <p>Hello User!</p>
       {/* If quiz data is present, show it, otherwise Loading */}
+      {console.log("this is quiz data hook content", quizData)}
       {quizData ? (<div><p>Quiz data is here: {quizData}</p></div>) : (<h1> Loading, Please wait...</h1>)}
     </div>
   );
