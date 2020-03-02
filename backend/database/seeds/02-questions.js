c530dfa5d58fa9f2075c579d698990f0012ca69d
Zolton <gmzesq@gmail.com>
2020-03-02 12:31:42 -0500

    Added difficulty column to questions table, to differentiate levels for users

--- backend/database/seeds/02-questions.js
+++ backend/database/seeds/02-questions.js
@@ -5,11 +5,11 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('questions').insert([
-        {question: "Where is the library?", author: 1},
-        {question: "Where is the hospital?", author: 1},
-        {question: "Where is the zen temple?", author: 1},
-        {question: "How are you doing?", author: 2},
-        {question: "That was funny", author: 3}
+        {question: "Where is the library?", author: 1, difficulty: "easy"},
+        {question: "Where is the hospital?", author: 1, difficulty: "easy"},
+        {question: "Where is the zen temple?", author: 1, difficulty: "medium"},
+        {question: "How are you doing?", author: 2, difficulty: "hard"},
+        {question: "That was funny", author: 3, difficulty: "expert"}
       ]);
     });
 };
