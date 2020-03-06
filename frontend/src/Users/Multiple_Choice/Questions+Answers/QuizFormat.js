df48bd89290eea8bb7279f65a8b897f01c506a1d
Zolton <gmzesq@gmail.com>
2020-03-06 11:39:13 -0500

    Got quizzes working!  Can display single question & relevant answers.  Next step is to break up into smaller components, add in ability to select answer and move onto next question

--- frontend/src/Users/Multiple_Choice/Questions+Answers/QuizFormat.js
+++ frontend/src/Users/Multiple_Choice/Questions+Answers/QuizFormat.js
@@ -2,24 +2,41 @@ import React, { useEffect } from "react";
 
 function QuizFormat(props) {
   const quizData = props.quizData;
+  console.log(quizData);
+  let currentIndex = 0;
 
-  console.log("formatting: ", quizData);
-
-  return (
-    <div>
-      Hello from formatting
-      {quizData ? (
-        quizData.map(data => (
-          <div>
-            <h2>{data.question}</h2>
-            <h2>{data.answer}</h2>
-          </div>
-        ))
-      ) : (
-        <h1>Loading, please wait</h1>
-      )}
-    </div>
-  );
+  // Check if props loaded yet
+  if (quizData) {
+    let quizQuestion = quizData[currentIndex].question;
+    let answerObject = quizData[currentIndex].answers;
+    return (
+      <div>
+        Hello from formatting
+        {console.log("index is ", quizData[currentIndex].answers)}
+        <h2>{quizQuestion}</h2>
+        {answerObject.map(singleAnswer => {
+          return (
+            <form>
+              <div className="radio">
+                <label>
+                  <input type="radio" value="options1" checked={false} />
+                  {singleAnswer.answer}
+                </label>
+              </div>
+            </form>
+          );
+        })}
+      </div>
+    );
+    // If props not loaded, keep waiting
+  } else {
+    return (
+      <div>
+        {/* Insert loading animation component here */}
+        Loading, please wait
+      </div>
+    );
+  }
 }
 
 export default QuizFormat;
