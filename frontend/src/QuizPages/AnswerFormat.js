b73d0327120e1218d0264129f85324947a0bda0d
Zolton <gmzesq@gmail.com>
2020-03-10 15:54:23 -0400

    Got all radio buttons working.  Got all Submit & Next Question buttons working as well

--- frontend/src/QuizPages/AnswerFormat.js
+++ frontend/src/QuizPages/AnswerFormat.js
@@ -1,21 +1,15 @@
 import React, { useState, useEffect } from "react";
 import QuestionButtons from "./QuestionButtons";
 
-
-
-function PassFail (answerID) {
-  return answerID
-
-}
-
 function AnswerFormat(props) {
-  const [answerID, setAnswerID] = useState();
-  const [answerTF, setAnswerTF] = useState()
-
-function setAnswers(id, TF) {
-    setAnswerID(id)
-    setAnswerTF(TF)
   
+  const [answerID, setAnswerID] = useState();
+  const [answerTF, setAnswerTF] = useState(null);
+
+  // Sets answerID and whether answer is correct in one function so onChange doesn't break
+  function setAnswers(id, TF) {
+    setAnswerID(id);
+    setAnswerTF(TF);
   }
 
   // Rename props for easier use
@@ -30,25 +24,31 @@ function setAnswers(id, TF) {
         {/* Dynamically generate radio buttons, regardless of # of answers */}
         {answerObject.map(singleAnswer => {
           return (
-            <div>
+            <div key = {singleAnswer.answer_id}>
               <input
                 type="radio"
                 id={singleAnswer.answer_id}
                 value={singleAnswer.answer}
-                onChange={()=>setAnswers(singleAnswer.answer_id, singleAnswer.correct_answer)}
-                // Checked works on an T/F basis.  If true, checked, if not, false
-                checked = {answerID == singleAnswer.answer_id}
+                onChange={() =>
+                  setAnswers(
+                    singleAnswer.answer_id,
+                    singleAnswer.correct_answer
+                  )
+                }
+                // Note: Checked works on an T/F basis.  If true, checked, if not, false
+                checked={answerID === singleAnswer.answer_id}
               />
               <label>{singleAnswer.answer}</label>
             </div>
-          )
+          );
         })}
       </form>
       <QuestionButtons
         setCurrentIndex={setCurrentIndex}
         currentIndex={currentIndex}
         quizLength={quizLength}
-        answer={PassFail(answerID)}
+        answerID={answerID}
+        answerTF={answerTF}
       />
     </div>
   );
